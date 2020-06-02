76c2c1fc9630904294c35d67cb92d6a3917765dc
exotiarg <juampaamendoza@gmail.com>
2020-06-02 19:31:02 -0300

    update

--- src/main.java
+++ src/main.java
@@ -3,6 +3,7 @@ public class main {
         System.out.println("Hello Wolrd");
         Box caja1 = new Box(4, "hola");
         Box caja2 = new Box(5, "holax");
+        Box caja3 = new Box(124124,"haghas");
         
     }
 }
