3fdbd0617f32d74e3ea83db2d681f97d29b9fd7f
exotiarg <juampaamendoza@gmail.com>
2020-06-02 19:33:32 -0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -4,6 +4,8 @@ public class main {
         Box caja1 = new Box(4, "hola");
         Box caja2 = new Box(5, "holax");
         Box caja3 = new Box(124124,"haghas");
+        Box caja4 = new Box(124124,"haghas");
+        
         
     }
 }
