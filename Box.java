b2b701ea861653b577fffc3c6d9b6257d00ba12b
exotiarg <juampaamendoza@gmail.com>
2020-06-02 19:29:31 -0300

    initial commit

---
+++ Box.java
@@ -0,0 +1,10 @@
+public class Box {
+    private int numero_letras;
+    String palabra;
+
+    public Box (int a, String b) {
+        numero_letras = a;
+        palabra = b;
+    };
+
+}
---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String args[]) {
+        System.out.println("Hello Wolrd");
+        Box caja1 = new Box(4, "hola");
+        Box caja2 = new Box(5, "holax");
+        
+    }
+}
+
